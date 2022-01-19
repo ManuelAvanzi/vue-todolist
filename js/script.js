3707eda5bfc86c9220a84953a5d446ad0cb2ecfe
ManuelAvanzi <manuel.92.avanzi@gmail.com>
2022-01-19 09:30:11 +0100

    Button add

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
 new Vue ({
     el:"#app",
     data:{
-
+        todo: [
+            'fare la spesa',
+            'fare i compiti',
+            'fare il bucato'
+        ],
+        temporaryItem: '',
     },
     methods:{
-
+        addItem:function(){
+            this.todo.push(this.temporaryItem);
+            console.log(this.todo);
+        }
     }
 })
